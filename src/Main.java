fca8fbb1ae0b5c694d9e05b658b79dcd04963ce7
Mazhar Altincay <mazharaltincay89@gmail.com>
2024-10-09 17:08:52 +0200

    jni java

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public
     public static void main(String[] args) {
         Main main = new Main();
         System.out.println(main.processInt(31));
+        System.out.println(main.processString("Hello Mazhar!"));
     }
     public native int processInt(int iVal);
+    public native String processString(String str);
 }
